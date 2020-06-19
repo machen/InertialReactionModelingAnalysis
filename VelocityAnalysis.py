46a285f457300e5da2b6a55090c876a645245bc8
machen <machen27@gmail.com>
2020-06-19 13:14:18 -0500

    Velocity analysis script
    Will do imports of Comsol data and then flow statistical analysis

---
+++ VelocityAnalysis.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import re
+
+""" Purpose of script is to import data for velocity analysis, but will also serve
+as a means of doing general data import from comsol. As a reminder, comsol files look like:
+"""
+
+
+workingDir = "."
