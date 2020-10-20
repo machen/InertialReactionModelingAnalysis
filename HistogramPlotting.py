119e377505e4c4bbb359e5221c8da25bfa840f36
machen <machen27@gmail.com>
2020-10-20 10:23:47 -0500

    New tasks

--- HistogramPlotting.py
+++ HistogramPlotting.py
@@ -14,6 +14,12 @@ CURRENTLY SWEEPS A WORKING FOLDER, TURN INTO A FUNCTION
 
 WOULD BE NICE TO MAKE A COMPARITOR FUNCTION THAT CAN TAKE TWO FILE NAMES (OR TWO COMPARISON DIRECTORIES)
 
+
+HEY SOME STUFF YOU NEED TO DO
+-Estimate a derivative of a given data set (np.diff(freq)/np.diff(valMean))
+-Calculate the mean of the distribution (integral of valMean*freq dval)
+-Calculate distribution median (point at which each area is divided half and half)
+
  """
 plt.rcParams['svg.fonttype'] = 'none'
 
@@ -52,12 +58,14 @@ def dataExtraction(workingDir, caseName, caseExt, smooth=False, window=5):
     return dataSets
 
 
-def dataSetPlot(dataSets, linestyle='-'):
+def dataSetPlot(dataSets, linestyle='-', smooth=0, fit=True):
     for key in dataSets:
         data = dataSets[key]
-        ax1.plot(data.valMean, data.normFreq, label=key, ls=linestyle)
+        ax1.plot(data.valMean, data.normFreq,
+                 label=key+'smooth {}'.format(smooth), ls=linestyle)
         ax2.plot(data.valMean,
-                 data.normFreq, label=key, ls=linestyle)
+                 data.normFreq, label=key+'smooth {}'.format(smooth),
+                 ls=linestyle)
 # HEY UNIFY THE NAMING IN THE MAIN SCRIPT SINCE IT'S NOT ALWAYS VELOCITIES
     return
 
@@ -70,32 +78,61 @@ def dataSmoothing(data, window=5):
 def genGaussian(mu, variance):
     sigma = np.sqrt(variance)
     x = np.linspace(mu-5*sigma, mu+5*sigma, 1000)
-    y = stats.norm.pdf(x,mu,sigma)
+    y = stats.norm.pdf(x, mu, sigma)
     return x, y
 
 
+def semilogYFitting(data, xPropName, yPropName, xRange):
+    subData = data.loc[(data.loc[:, xPropName] >= xRange[0]) & (data.loc[:, xPropName] <= xRange[1]), :]
+    xData = subData.loc[:, xPropName].values
+    yData = np.log10(subData.loc[:, yPropName].values)
+    fit = np.polyfit(xData, yData, 1)
+    yEst = np.power(10, np.polyval(fit, xData))
+    return fit, xData, yEst
+
 window = 5
 smooth = True
+fitRange = np.array([85, 90])
+#fitRange = np.array([65, 85])
 
-workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\FlowData\\Pillar gap-angle-180 linear bins\\"
-#workingDirA = "..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar Region - Angle - 180 linear bins"
+workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\FlowData\\Pillar Gap-angle-180 linear bins\\"
+#workingDirA = "..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar Gap -angle- 180 linear bins"
+#workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\ChemData\\Pillar gap-cProduct-100 linear bins\\"
 # workingDir = "."
-caseNameA = "TwoInletsTwoColumns_v5.2_ExF_FlowOnly_GapVarSmall_"
-caseExtA = "Re50.flowdata_histogram\.csv"
-filepath_or_buffer
+caseNameA = "TwoInletsTwoColumns_v5.2_ExF_r100_d100"
+caseExtA = ".chemdata_histogram\.csv"
 # workingDirB = "..\\..\\..\\..\\..\\Multipillar\\Normal\\FlowData_Normal\\200 log bins - 250 to -2500"
 workingDirB = "..\\..\\..\\..\\..\\..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar gap-angle-180 linear bins"
-caseNameB = "TwoInletsTwoColumns_v5.2_ExF_FlowOnly_GapVarSmall_"
-caseExtB = "Re50.flowdata_histogram\.csv"
+#workingDirB = "..\\..\\..\\..\\..\\..\\Comsol5.5\\TwoPillars\\ExF\\ChemData\\Pillar gap-cProduct-100 linear bins\\"
+caseNameB = "TwoInletsTwoColumns_v5.2_ExF_r100_d100"
+caseExtB = ".chemdata_histogram\.csv"
 
 # Plot for everything
 f1, ax1 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
 f2, ax2 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
 
+
 dataSetA = dataExtraction(workingDirA, caseNameA, caseExtA, smooth, window)
-dataSetPlot(dataSetA)
+dataSetPlot(dataSetA, smooth=window)
+# for key in dataSetA:
+#     fit, xVal, yRes = semilogYFitting(dataSetA[key], 'valMean',
+#                                       'normFreq', np.array([87, 91]))
+#     ax1.plot(xVal, yRes, ls='-', color='k', label='A'+str(fit))
+#     ax2.plot(xVal, yRes, ls='-', color='k', label='A'+str(fit))
+#     fit, xVal, yRes = semilogYFitting(dataSetA[key], 'valMean',
+#                                       'normFreq', np.array([75, 87]))
+#     ax1.plot(xVal, yRes, ls='-', color='k', label='A'+str(fit))
+#     ax2.plot(xVal, yRes, ls='-', color='k', label='A'+str(fit))
+# for i in [2,4,5,10]:
+#     dataSetSmooth = dataExtraction('.', caseNameA, caseExtA, smooth=True, window=i)
+#     dataSetPlot(dataSetSmooth, smooth=i)
 dataSetB = dataExtraction(workingDirB, caseNameB, caseExtB, smooth, window)
-dataSetPlot(dataSetB)
+dataSetPlot(dataSetB, smooth=window)
+# for key in dataSetB:
+#     fit, xVal, yRes = semilogYFitting(dataSetB[key], 'valMean',
+#                                       'normFreq', np.array([75, 91]))
+#     ax1.plot(xVal, yRes, ls='--', color='k', label='B'+str(fit))
+#     ax2.plot(xVal, yRes, ls='--', color='k', label='B'+str(fit))
 # for i in range(10):
 #     xGauss, yGauss = genGaussian(90, i)
 #     ax1.plot(xGauss, yGauss, ls='--', color='k', label='{} gaussian'.format(i))
@@ -110,7 +147,7 @@ ax2.set_ylabel("Normalized freq.")
 ax1.legend(loc=0)
 ax1.set_yscale('log')
 ax2.legend(loc=0)
-plt.yscale('log')
-plt.xscale('log')
+# plt.yscale('log')
+# plt.xscale('log')
 plt.ion()
 plt.show()
