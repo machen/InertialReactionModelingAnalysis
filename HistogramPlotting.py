33294f1084a011b9a575ec44ef89675238f69289
machen <machen27@gmail.com>
2020-10-22 14:49:22 -0500

    Statistical analysis and metadata analysis
    Script adjusted to calculate statistics on PDFs and to plot meta analysis based on collected data

--- HistogramPlotting.py
+++ HistogramPlotting.py
@@ -16,9 +16,10 @@ WOULD BE NICE TO MAKE A COMPARITOR FUNCTION THAT CAN TAKE TWO FILE NAMES (OR TWO
 
 
 HEY SOME STUFF YOU NEED TO DO
--Estimate a derivative of a given data set (np.diff(freq)/np.diff(valMean))
--Calculate the mean of the distribution (integral of valMean*freq dval)
+
 -Calculate distribution median (point at which each area is divided half and half)
+-create a plot that compares various aggergate statistics to a given parameter
+
 
  """
 plt.rcParams['svg.fonttype'] = 'none'
@@ -28,14 +29,19 @@ def extractParams(fileName):
     # Produces a dictionary of experimental parameters
     r1Pat = re.compile('r1_(\d+?)_')
     r2Pat = re.compile('r2_(\d+?)_')
-    rePat = re.compile('Re(.*?)_')
+    rePat = re.compile('Re(.*?).(chem)|(flow)data.txt')
     dPat = re.compile('d(\d+?)_')
+    stokesPat = re.compile('_Stokes_')
     r1Val = re.search(r1Pat, fileName).group(1)
     r2Val = re.search(r2Pat, fileName).group(1)
     dVal = re.search(dPat, fileName).group(1)
     reVal = re.search(rePat, fileName).group(1)
+    if re.search(stokesPat, fileName):
+        flow = 'Stokes'
+    else:
+        flow = 'NS'
     res = {'r1': float(r1Val), 'r2': float(r2Val), 'd': float(dVal),
-           'Re': float(reVal)}
+           'Re': float(reVal), 'Flow': flow}
     return res
 
 
@@ -58,21 +64,49 @@ def dataExtraction(workingDir, caseName, caseExt, smooth=False, window=5):
     return dataSets
 
 
-def dataSetPlot(dataSets, linestyle='-', smooth=0, fit=True):
+def dataSetPlot(dataSets, metaData, linestyle='-', smooth=0, fit=True):
     for key in dataSets:
         data = dataSets[key]
-        ax1.plot(data.valMean, data.normFreq,
-                 label=key+'smooth {}'.format(smooth), ls=linestyle)
-        ax2.plot(data.valMean,
-                 data.normFreq, label=key+'smooth {}'.format(smooth),
-                 ls=linestyle)
+        params = extractParams(key)
+        dataMean, dataVar = pdfStats(data)
+        params['PDFmean'] = dataMean
+        params['PDFstd'] = np.sqrt(dataVar)
+        metaData = metaData.append(params, ignore_index=True)
+        dataMid = np.mean([np.min(data.normFreq), np.max(data.normFreq)])
+        a1 = ax1.plot(data.valMean, data.normFreq,
+                      label=key+'smooth {}'.format(smooth), ls=linestyle)
+        c = a1[0].get_color()
+        ax1.plot([dataMean, dataMean],
+                 [np.min(data.normFreq), np.max(data.normFreq)],
+                 ls='-', color=c)
+        # ax1.plot([dataMean-np.sqrt(dataVar), dataMean+np.sqrt(dataVar)],
+        #          [dataMid, dataMid], ls='--', color=c)
+
+        a2 = ax2.plot(data.valMean,
+                      data.normFreq, label=key+'smooth {}'.format(smooth),
+                      ls=linestyle)
+        c = a2[0].get_color()
+        ax2.plot([dataMean, dataMean],
+                 [np.min(data.normFreq), np.max(data.normFreq)],
+                 ls='-', color=c)
+        # ax2.plot([dataMean-np.sqrt(dataVar), dataMean+np.sqrt(dataVar)],
+        #          [dataMid, dataMid], ls='--', color=c)
+        diffPDF = np.diff(data.normFreq)/np.diff(data.valMean)
+        ax3.plot(data.valMean[1:], diffPDF, label=key, ls=linestyle)
 # HEY UNIFY THE NAMING IN THE MAIN SCRIPT SINCE IT'S NOT ALWAYS VELOCITIES
-    return
+    return metaData
 
 
 def dataSmoothing(data, window=5):
     dataSmooth = data.rolling(window, center=True).mean()
     return dataSmooth
+
+
+def pdfStats(data):
+    dx = data.rightBin-data.leftBin
+    eVal = np.sum(data.valMean*data.normFreq*dx)
+    varVal = np.sum(data.normFreq*(data.valMean-eVal)**2*dx)
+    return eVal, varVal
 
 
 def genGaussian(mu, variance):
@@ -90,30 +124,39 @@ def semilogYFitting(data, xPropName, yPropName, xRange):
     yEst = np.power(10, np.polyval(fit, xData))
     return fit, xData, yEst
 
-window = 5
-smooth = True
-fitRange = np.array([85, 90])
-#fitRange = np.array([65, 85])
 
-workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\FlowData\\Pillar Gap-angle-180 linear bins\\"
-#workingDirA = "..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar Gap -angle- 180 linear bins"
-#workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\ChemData\\Pillar gap-cProduct-100 linear bins\\"
+def metaPlot(metaData, prop='Re', flowCond='NS'):
+    subData = metaData.loc[metaData.loc[:, 'Flow'] == flowCond, :]
+    subData.sort_values(by=prop)
+    ax4.plot(subData.loc[:, prop], subData.loc[:, 'PDFmean'], ls='none', marker='o')
+    return
+
+
+window = 2
+smooth = False
+fitRange = np.array([85, 90])
+# fitRange = np.array([65, 85])
+# workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\FlowData\\Pillar gap-angle-180 linear bins\\"
+# workingDirA = "..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar Gap -angle- 180 linear bins"
+workingDirA = "..\\Comsol5.4\\TwoPillars\\Version5\\ExF\\ChemData\\Pillar gap-dCdt-100 linear bins\\"
 # workingDir = "."
-caseNameA = "TwoInletsTwoColumns_v5.2_ExF_r100_d100"
+caseNameA = "TwoInletsTwoColumns_v5.2_ExF_r1_100_r2_100_d100"
 caseExtA = ".chemdata_histogram\.csv"
 # workingDirB = "..\\..\\..\\..\\..\\Multipillar\\Normal\\FlowData_Normal\\200 log bins - 250 to -2500"
-workingDirB = "..\\..\\..\\..\\..\\..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar gap-angle-180 linear bins"
-#workingDirB = "..\\..\\..\\..\\..\\..\\Comsol5.5\\TwoPillars\\ExF\\ChemData\\Pillar gap-cProduct-100 linear bins\\"
-caseNameB = "TwoInletsTwoColumns_v5.2_ExF_r100_d100"
+# workingDirB = "..\\..\\..\\..\\..\\..\\Comsol5.5\\TwoPillars\\ExF\\FlowDatawVorticity\\Pillar gap-angle-180 linear bins"
+workingDirB = "..\\..\\..\\..\\..\\..\\Comsol5.5\\TwoPillars\\ExF\\ChemData\\Pillar gap-dCdt-100 linear bins\\"
+caseNameB = "TwoInletsTwoColumns_v5.2_ExF_r1_100_r2_100_d100"
 caseExtB = ".chemdata_histogram\.csv"
 
 # Plot for everything
 f1, ax1 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
 f2, ax2 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
+f3, ax3 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
+f4, ax4 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
 
-
+metaData = pd.DataFrame([], columns=['r1', 'r2', 'd', 'Re', 'Flow', 'PDFmean', 'PDFstd'])
 dataSetA = dataExtraction(workingDirA, caseNameA, caseExtA, smooth, window)
-dataSetPlot(dataSetA, smooth=window)
+metaData = dataSetPlot(dataSetA, metaData, smooth=window)
 # for key in dataSetA:
 #     fit, xVal, yRes = semilogYFitting(dataSetA[key], 'valMean',
 #                                       'normFreq', np.array([87, 91]))
@@ -127,7 +170,7 @@ dataSetPlot(dataSetA, smooth=window)
 #     dataSetSmooth = dataExtraction('.', caseNameA, caseExtA, smooth=True, window=i)
 #     dataSetPlot(dataSetSmooth, smooth=i)
 dataSetB = dataExtraction(workingDirB, caseNameB, caseExtB, smooth, window)
-dataSetPlot(dataSetB, smooth=window)
+metaData = dataSetPlot(dataSetB, metaData, smooth=window)
 # for key in dataSetB:
 #     fit, xVal, yRes = semilogYFitting(dataSetB[key], 'valMean',
 #                                       'normFreq', np.array([75, 91]))
@@ -138,8 +181,11 @@ dataSetPlot(dataSetB, smooth=window)
 #     ax1.plot(xGauss, yGauss, ls='--', color='k', label='{} gaussian'.format(i))
 #     ax2.plot(xGauss, yGauss, ls='--', color='k', label='{} gaussian'.format(i))
 
+metaPlot(metaData)
+
 ax1.set_title("PDFs")
 ax2.set_title("PDFs")
+ax3.set_title("Differentiated PDF")
 ax1.set_xlabel("Value")
 ax1.set_ylabel("Normalized freq.")
 ax2.set_xlabel("Value")
@@ -147,6 +193,7 @@ ax2.set_ylabel("Normalized freq.")
 ax1.legend(loc=0)
 ax1.set_yscale('log')
 ax2.legend(loc=0)
+ax3.legend(loc=0)
 # plt.yscale('log')
 # plt.xscale('log')
 plt.ion()
