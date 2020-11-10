6135f4f8e6616ff8dc84aeb29778abb020065348
machen <machen27@gmail.com>
2020-11-10 14:43:52 -0600

    Histograms and histogram plotting

--- ImageHistogram.py
+++ ImageHistogram.py
@@ -13,8 +13,6 @@ Things I need to do:
 
 -To prep for analysis, I need to also do the statistical calculations I
 used in the sim analysis suite
--Also, probably useful to window my data (may be possible to eliminate background?)
-Could avoid with log PDF
 
 
 """
@@ -33,36 +31,102 @@ def pdfStats(data):
     return eVal, varVal
 
 
+def subSelectData(data, xRange=None, yRange=None):
+    """Assumes that each of the inputs to the function is a tuple containing
+     max and min values of x, y, and z that we wish to include.
+
+     Assumes that we're working with images. You should 100% pre run without any
+     subselection of data to determine the correct area"""
+    if xRange:
+        data = data[:, xRange[0]:xRange[1]]
+    if yRange:
+        data = data[yRange[0]:yRange[1], :]
+    return data
+
+
+def genOutputFolderAndParams(dataDir, case, nBins, maxNorm,
+                             imageDict=None,
+                             regionName='Result', dataRegionX=None,
+                             dataRegionY=None, dataRegionZ=None):
+    # Creates a new folder and populates it with key binning info
+    if maxNorm:
+        outputPath = '..\\{} max norm {} bins\\'.format(regionName, nBins)
+    else:
+        outputPath = '..\\{} {} bins\\'.format(regionName, nBins)
+    outputFile = outputPath+'Parameters.txt'
+    if not os.path.isdir(outputPath):
+        os.mkdir(outputPath)
+    with open(outputFile, "w") as outFile:
+        outFile.write("Data directory: {}\n".format(dataDir))
+        outFile.write("Case extension: {}\n".format(case))
+        outFile.write("Number of bins: {}\n".format(nBins))
+        outFile.write("Image key: {}\n".format(imageDict))
+        outFile.write("Maximum Normalization: {}\n".format(maxNorm))
+        outFile.write("X Region: {}\n".format(dataRegionX))
+        outFile.write("Y Region: {}\n".format(dataRegionY))
+        outFile.write("Z Region: {}\n".format(dataRegionZ))
+    return outputPath
+
+
+def producePDF(file, imageDict, outFile, maxNorm, bins=100, xRange=None, yRange=None):
+    img = Image.open(file)
+    imgList = []
+    for key in imageDict:
+        img.seek(imageDict[key])
+        data = np.array(img)
+        if maxNorm:  # If using maximum normalization, all values scaled to largest value
+            data = data/np.max(data)
+        data = subSelectData(data, xRange=xRange, yRange=yRange)
+        dataPdf, dataVal, dataLeft, dataRight = genPDF(data, bins)
+        dataDict = {'normFreq': dataPdf, 'valMean': dataVal,
+                    'leftBin': dataLeft, 'rightBin': dataRight}
+        dataDF = pd.DataFrame(dataDict)
+        dataDF.to_csv(outFile+file[:-4]+'_{}_hist.csv'.format(key))
+        imgList.append(Image.fromarray(data))
+    imgList[0].save(outFile+file[:-4]+'.tiff', compression='tiff_deflate',
+                    save_all=True, append_images=imgList[1:])
+    return
+
+
 workingDir = "G:\\My Drive\\Postdoctoral work\\Inertial flow study\\Experiments\\2PillarD-1_P2_A2\\DataProcessing\\"
 os.chdir(workingDir)
 filePat = re.compile('.*\.tif')
 bins = 100
+xRange = [830, 1155]  # Should be matrix indices for the given image
+yRange = [900, 1350]  # Should be matrix indices for the given image
+maxNorm = True
 
 fileList = os.listdir()
-
+# Links identifier to stack position, also calls what images will be binned
+imageDict = {'bright': 0, 'dark': 1, 'fluor': 2}
+outFile = genOutputFolderAndParams(workingDir, filePat, bins, maxNorm,
+                                   imageDict=imageDict, dataRegionX=xRange,
+                                   dataRegionY=yRange)
 
 for file in os.listdir():
     if re.match(filePat, file):
         print(file)
-        img = Image.open(file)
-        img.seek(0)
-        bright = np.array(img)
-        brightPdf, brightVal, brightLeft, brightRight = genPDF(bright, bins)
-        brightDict = {'normFreq': brightPdf, 'meanVal': brightVal,
-                      'leftBin': brightLeft, 'rightBin': brightRight}
-        brightDF = pd.DataFrame(brightDict)
-        brightDF.to_csv(workingDir+file[:-4]+'_brightfield_histogram.csv')
-        img.seek(1)
-        dark = np.array(img)
-        darkPdf, darkVal, darkLeft, darkRight = genPDF(dark, bins)
-        darkDict = {'normFreq': darkPdf, 'meanVal': darkVal,
-                    'leftBin': darkLeft, 'rightBin': darkRight}
-        darkDF = pd.DataFrame(darkDict)
-        darkDF.to_csv(workingDir+file[:-4]+'_dark_histogram.csv')
-        img.seek(2)
-        fluor = np.array(img)
-        fluorPdf, fluorVal, fluorLeft, fluorRight = genPDF(fluor, bins)
-        fluorDict = {'normFreq': fluorPdf, 'meanVal': fluorVal,
-                     'leftBin': fluorLeft, 'rightBin': fluorRight}
-        fluorDF = pd.DataFrame(fluorDict)
-        fluorDF.to_csv(workingDir+file[:-4]+'_fluorescence_histogram.csv')
+        producePDF(file, imageDict, outFile, maxNorm,
+                   bins=100, xRange=xRange, yRange=yRange)
+        # img = Image.open(file)
+        # img.seek(0)
+        # bright = np.array(img)
+        # brightPdf, brightVal, brightLeft, brightRight = genPDF(bright, bins)
+        # brightDict = {'normFreq': brightPdf, 'meanVal': brightVal,
+        #               'leftBin': brightLeft, 'rightBin': brightRight}
+        # brightDF = pd.DataFrame(brightDict)
+        # brightDF.to_csv(workingDir+file[:-4]+'_brightfield_histogram.csv')
+        # img.seek(1)
+        # dark = np.array(img)
+        # darkPdf, darkVal, darkLeft, darkRight = genPDF(dark, bins)
+        # darkDict = {'normFreq': darkPdf, 'meanVal': darkVal,
+        #             'leftBin': darkLeft, 'rightBin': darkRight}
+        # darkDF = pd.DataFrame(darkDict)
+        # darkDF.to_csv(workingDir+file[:-4]+'_dark_histogram.csv')
+        # img.seek(2)
+        # fluor = np.array(img)
+        # fluorPdf, fluorVal, fluorLeft, fluorRight = genPDF(fluor, bins)
+        # fluorDict = {'normFreq': fluorPdf, 'meanVal': fluorVal,
+        #              'leftBin': fluorLeft, 'rightBin': fluorRight}
+        # fluorDF = pd.DataFrame(fluorDict)
+        # fluorDF.to_csv(workingDir+file[:-4]+'_fluorescence_histogram.csv')
--- ImageHistogramPlotting.py
+++ ImageHistogramPlotting.py
@@ -5,3 +5,134 @@ import matplotlib.pyplot as plt
 import os
 import scipy.stats as stats
 # import seaborn as sns
+
+
+def extractParams(fileName):
+    # Produces a dictionary of experimental parameters
+    qPat = re.compile('(\d*\.?\d*)q')
+    repPat = re.compile('(\d{0,3})_dark_hist.csv')
+    qVal = re.search(qPat, fileName)
+    repVal = re.search(repPat, fileName)
+    res = {'q': float(qVal.group(1)), 'replicate': repVal.group(1)}
+    return res
+
+
+def dataExtraction(workingDir, caseName, caseExt, smooth=False, window=5):
+    os.chdir(workingDir)
+    filePat = re.compile(caseName+'.*?'+caseExt)
+    fileList = os.listdir('.')
+    dataSets = {}
+    for fileName in fileList:
+        if re.match(filePat, fileName):
+            print(fileName)
+            # params = extractParams(fileName)
+            data = pd.read_csv(fileName, header=0)
+            if smooth:
+                # Smooth data with rolling average of size window
+                data = dataSmoothing(data, window)
+                # Drop NaNs
+                data = data.dropna()
+            dataSets[fileName] = data
+    return dataSets
+
+
+def dataSetPlot(dataSets, metaData, linestyle='-', smooth=0, fit=True):
+    for key in dataSets:
+        data = dataSets[key]
+        params = extractParams(key)
+        dataMean, dataVar = pdfStats(data)
+        params['PDFmean'] = dataMean
+        params['PDFstd'] = np.sqrt(dataVar)
+        metaData = metaData.append(params, ignore_index=True)
+        a1 = ax1.plot(data.valMean, data.normFreq,
+                      label=key+'smooth {}'.format(smooth), ls=linestyle)
+        c = a1[0].get_color()
+        ax1.plot([dataMean, dataMean],
+                 [np.min(data.normFreq), np.max(data.normFreq)],
+                 ls='-', color='k')
+        # ax1.plot([dataMean-np.sqrt(dataVar), dataMean+np.sqrt(dataVar)],
+        #          [dataMid, dataMid], ls='--', color=c)
+
+        a2 = ax2.plot(data.valMean,
+                      data.normFreq, label=key+'smooth {}'.format(smooth),
+                      ls=linestyle)
+        c = a2[0].get_color()
+        ax2.plot([dataMean, dataMean],
+                 [np.min(data.normFreq), np.max(data.normFreq)],
+                 ls='-', color='k')
+        # ax2.plot([dataMean-np.sqrt(dataVar), dataMean+np.sqrt(dataVar)],
+        #          [dataMid, dataMid], ls='--', color=c)
+        diffPDF = np.diff(data.normFreq)/np.diff(data.valMean)
+        ax3.plot(data.valMean[1:], diffPDF, label=key, ls=linestyle)
+    return metaData
+
+
+def dataSmoothing(data, window=5):
+    dataSmooth = data.rolling(window, center=True).mean()
+    return dataSmooth
+
+
+def pdfStats(data):
+    dx = data.rightBin-data.leftBin
+    eVal = np.sum(data.valMean*data.normFreq*dx)
+    varVal = np.sum(data.normFreq*(data.valMean-eVal)**2*dx)
+    return eVal, varVal
+
+
+def genGaussian(mu, variance):
+    sigma = np.sqrt(variance)
+    x = np.linspace(mu-5*sigma, mu+5*sigma, 1000)
+    y = stats.norm.pdf(x, mu, sigma)
+    return x, y
+
+
+def metaPlot(metaData, prop='q'):
+    # Change me to work with the image histograms and metadata
+    metaData.sort_values(by=prop)
+    ax4.errorbar(metaData.loc[:, prop], metaData.loc[:, 'PDFmean'],
+                 yerr=metaData.loc[:, 'PDFstd'], ls='none', marker='o',
+                 capsize=2)
+    return
+
+
+plt.rcParams['svg.fonttype'] = 'none'
+smooth = False
+window = 10
+
+
+workingDirA = "G:\\My Drive\\Postdoctoral work\\Inertial flow study\\Experiments\\2PillarD-1_P2_A2\\Result 100 bins\\"
+os.chdir(workingDirA)
+caseNameA = '\d*q_Pillars\d*'
+
+caseExtA = ".*dark_hist\.csv"
+
+f1, ax1 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
+f2, ax2 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
+f3, ax3 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
+f4, ax4 = plt.subplots(1, 1, sharex='col', figsize=(12, 10))
+
+metaData = pd.DataFrame([], columns=['q', 'replicate', 'PDFmean', 'PDFstd'])
+dataSetA = dataExtraction(workingDirA, caseNameA, caseExtA, smooth, window)
+metaData = dataSetPlot(dataSetA, metaData, smooth=window)
+metaPlot(metaData)
+# dataSetB = dataExtraction(workingDirB, caseNameB, caseExtB, smooth, window)
+# metaData = dataSetPlot(dataSetB, metaData, smooth=window)
+
+ax1.set_title("PDFs")
+ax2.set_title("PDFs")
+ax3.set_title("Differentiated PDF")
+ax1.set_xlabel("Value")
+ax1.set_ylabel("Normalized freq.")
+ax2.set_xlabel("Value")
+ax2.set_ylabel("Normalized freq.")
+ax1.legend(loc=0)
+ax1.set_yscale('log')
+ax2.legend(loc=0)
+ax3.legend(loc=0)
+ax4.set_xlabel('q')
+ax4.set_ylabel('Mean of PDF')
+ax3.set_yscale('log')
+# plt.xscale('log')
+ax4.legend(loc=0)
+plt.ion()
+plt.show()
