052c7c7c528f3ce06037a9d4ec24d85d947eac82
machen <machen27@gmail.com>
2023-05-30 09:31:11 -0500

    Turbo colormap for better visualization, plots

--- ToyModel.py
+++ ToyModel.py
@@ -5,6 +5,9 @@ import seaborn as sns
 """Script to produce model results for a stirred mixed reactor"""
 
 plt.ion()
+sns.set_context('poster',font_scale=1.25)
+plt.rcParams['font.family'] = 'Cambria'
+plt.rcParams['svg.fonttype'] = 'none'
 
 tRes = np.logspace(-6,6,130)
 # Stirred reactor at steady state
@@ -15,18 +18,14 @@ cA = cA0/(1+k*cB0*tRes)  # Normalized to
 Da = k*cA0*tRes
 r = k*cB0*cA
 
-f1, ax1 = plt.subplots(1,1)
-ax1.plot(tRes, cA/cA0)
-plt.xlabel('Residence Time (sec)')
-plt.ylabel('Normalized Reactant concentration (-)')
-plt.xscale('log')
-sns.despine(f1)
-
-f2,ax2 = plt.subplots(1,1)
-ax2.plot(Da,k*cB0*cA/(k*cB0*cA0))
+f1, ax1 = plt.subplots(1,1,figsize=(12,10))
+#ax1.plot(tRes, cA/cA0, label='Reactant Concentration')
+ax1.plot(Da,k*cB0*cA/(k*cB0*cA0), label='Reaction Rate')
+ax1.plot(Da, 1-cA/cA0, label = 'Product')
 plt.xlabel('Dahmkohler Number')
-plt.ylabel('Normalized Reaction Rate (-)')
+plt.ylabel('Normalized Value (-)')
 plt.xscale('log')
-sns.despine(f2)
+plt.legend()
+sns.despine(f1)
 
 plt.show()
